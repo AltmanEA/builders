15e4e06128ccc0d1d728efa2baac1dcb25ea82af
AltmanEvgenii <AltmanEA@gmail.com>
2022-08-10 19:27:58 +0600

    fun add

--- src/main/kotlin/HBuilder.kt
+++ src/main/kotlin/HBuilder.kt
@@ -1,11 +1,13 @@
+import HElement.Companion.div
+import HElement.Companion.p
 import java.io.File
 
 fun main(){
-    val div = HElement(
-        "div",
-        mapOf("id" to "root"),
-        "Hello"
+    val div = div(
+        "Hello",
+        mapOf("id" to "root")
     )
+    div.add(p("in p"))
 
     File("result.html").printWriter().use{
         it.print(
--- src/main/kotlin/HElement.kt
+++ src/main/kotlin/HElement.kt
@@ -1,10 +1,21 @@
 class HElement(
     val name: String,
-    val attributes: Map<String, String>,
-    val content: String
+    var content: String,
+    val attributes: Map<String, String> = emptyMap()
 ) {
+    fun add(hElement: HElement) {
+        content += hElement.toString()
+    }
+
     val attrs = attributes.toList().joinToString { "${it.first}=${it.second}" }
 
     override fun toString()=
         "<$name $attrs>$content</$name>"
+
+    companion object {
+        fun div(content: String, attributes: Map<String, String> = emptyMap()) =
+            HElement("div", content, attributes)
+        fun p(content: String, attributes: Map<String, String> = emptyMap()) =
+            HElement("p", content, attributes)
+    }
 }
